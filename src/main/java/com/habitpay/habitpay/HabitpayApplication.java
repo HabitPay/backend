988fb885facca04476620cf671176ccbe02d8dd7
joonhan <joonhan@student.42seoul.kr>
2024-01-03 15:57:53 +0900

    first commit

---
+++ src/main/java/com/habitpay/habitpay/HabitpayApplication.java
@@ -0,0 +1,13 @@
+package com.habitpay.habitpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habitpay/habitpay/HabitpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitpay.habitpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
