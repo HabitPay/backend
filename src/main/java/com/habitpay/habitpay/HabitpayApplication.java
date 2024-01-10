aab605f7cd44638acf5ab64e58f58a78d9a30697
joonhan <joonhan@student.42seoul.kr>
2024-01-10 18:06:49 +0900

    ✨ feat: Spring Security 기본 로그인 페이지 제거 및 CORS 해결

--- src/main/java/com/habitpay/habitpay/HabitpayApplication.java
+++ src/main/java/com/habitpay/habitpay/HabitpayApplication.java
@@ -2,12 +2,28 @@ package com.habitpay.habitpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@EnableJpaAuditing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HabitpayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HabitpayApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
