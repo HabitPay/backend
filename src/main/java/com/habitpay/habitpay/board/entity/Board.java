d9e6f0c9f24dd199bc93b44188009f2420840d72
joonhan <joonhan@student.42seoul.kr>
2024-01-10 18:06:05 +0900

    ✨ feat: Add Board entity

---
+++ src/main/java/com/habitpay/habitpay/board/entity/Board.java
@@ -0,0 +1,40 @@
+package com.habitpay.habitpay.board.entity;
+
+import com.habitpay.habitpay.Timestamped;
+import com.habitpay.habitpay.board.dto.BoardRequestDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity(name = "boards")
+public class Board extends Timestamped {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column()
+    private String password;
+
+    public Board(BoardRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+        this.password = requestDto.getPassword();
+    }
+
+    public void update(BoardRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+        this.password = requestDto.getPassword();
+    }
+}
