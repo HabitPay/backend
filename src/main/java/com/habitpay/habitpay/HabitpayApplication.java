a3ab13d4f08b5f62cf520a0736add0faf0c76934
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-03-04 17:49:59 +0900

    chore: Initialize project files

--- src/main/java/com/habitpay/habitpay/HabitpayApplication.java
+++ src/main/java/com/habitpay/habitpay/HabitpayApplication.java
@@ -1,29 +1,13 @@
-package com.habitpay.habitpay;
+package com.habitpay.HabitPay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableJpaAuditing
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class HabitpayApplication {
+@SpringBootApplication
+public class HabitPayApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(HabitpayApplication.class, args);
-	}
-
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-			}
-		};
+		SpringApplication.run(HabitPayApplication.class, args);
 	}
 
 }
--- src/test/java/com/habitpay/habitpay/HabitpayApplicationTests.java
+++ src/test/java/com/habitpay/habitpay/HabitpayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.habitpay.habitpay;
+package com.habitpay.HabitPay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HabitpayApplicationTests {
+class HabitPayApplicationTests {
 
 	@Test
 	void contextLoads() {
