74ffa9966dc9ea23f5afc9717c246eb44e1a1e20
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-03-11 00:22:39 +0900

    ✨ feat: Implement Google OAuth2 login #5
    - 세션 방식으로 구현함.
    - JWT 토큰 발급 방식은 나중에 구현 예정
    - 테이블에 데이터 저장하는 것까지 확인함

--- src/main/java/com/habitpay/habitpay/HabitpayApplication.java
+++ src/main/java/com/habitpay/habitpay/HabitpayApplication.java
@@ -2,7 +2,9 @@ package com.habitpay.habitpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class HabitpayApplication {
 
---
+++ src/main/java/com/habitpay/habitpay/user/enums/Role.java
@@ -0,0 +1,14 @@
+package com.habitpay.habitpay.user.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    GUEST("ROLE_GUEST", "Guest"),
+    USER("ROLE_USER", "Common User");
+
+    private final String key;
+    private final String title;
+}
