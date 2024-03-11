f3e5216ba362fa3871a73ed67a2c0626f7c1004d
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-03-11 20:31:58 +0900

    👷 chore: Replace entrypoint command for hot reload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-./gradlew getDependencies
-
-# Start spring boot devtools process in the background.
-# This is necessary for hot reload.
-(./gradlew -t :bootJar) &
-# Next, start the app. 
-# The "PskipDownload" option ensures dependencies are not downloaded again.
-./gradlew bootRun -PskipDownload=true
+./gradlew --no-daemon --build-cache -t --watch-fs bootRun
