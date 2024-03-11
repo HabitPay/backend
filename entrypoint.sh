7f432ed5b7e9b442a92bb9fd24d4248e3a67f353
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-03-11 23:01:55 +0900

    👷 chore: Add stopping busy gradle daemon before bootRun

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-./gradlew --no-daemon --build-cache -t --watch-fs bootRun
+status_output=$(./gradlew --status)
+pid=$(echo "$status_output" | grep "BUSY" | awk '{print $1}')
+
+if [ -n "$pid" ]; then
+    echo "Gradle Daemon (PID: $pid) is busy, stopping..."
+    kill -9 $pid
+    sleep 1  # Daemon이 완전히 종료되기를 기다립니다.
+else
+    echo "No active Gradle Daemon found."
+fi
+
+./gradlew --status
+
+./gradlew --build-cache --parallel -t --watch-fs bootRun
