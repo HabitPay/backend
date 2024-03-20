af89eeb735a13fda4df7bbde1a17142f88a23418
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-03-20 13:58:20 +0900

    :fire: remove: Remove name field in SessionUser.java #14

--- src/main/java/com/habitpay/habitpay/global/config/auth/dto/SessionUser.java
+++ src/main/java/com/habitpay/habitpay/global/config/auth/dto/SessionUser.java
@@ -7,11 +7,9 @@ import java.io.Serializable;
 
 @Getter
 public class SessionUser implements Serializable {
-    private String name;
     private String email;
 
     public SessionUser(Member member) {
-        this.name = member.getName();
         this.email = member.getEmail();
     }
 }
