c85d3b843b39b1f26c5e4a2ea1f389748e4b2357
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-03-21 23:04:10 +0900

    :sparkles: feat: Add MemberService #14

--- src/main/java/com/habitpay/habitpay/domain/member/api/MemberApi.java
+++ src/main/java/com/habitpay/habitpay/domain/member/api/MemberApi.java
@@ -1,45 +1,50 @@
 package com.habitpay.habitpay.domain.member.api;
 
-import com.habitpay.habitpay.domain.member.dao.MemberRepository;
+import com.habitpay.habitpay.domain.member.application.MemberService;
 import com.habitpay.habitpay.domain.member.domain.Member;
 import com.habitpay.habitpay.domain.member.dto.MemberRequest;
 import com.habitpay.habitpay.global.config.jwt.TokenProvider;
-import com.habitpay.habitpay.global.util.CookieUtil;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.StringTokenizer;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class MemberApi {
-    private final MemberRepository repository;
+    private final MemberService memberService;
     private final TokenProvider tokenProvider;
 
     @PostMapping("/member")
     @ResponseStatus(HttpStatus.CREATED)
-    public void updateMember(@RequestBody MemberRequest memberRequest, HttpServletRequest request) {
-        String email = tokenProvider.getEmail(CookieUtil.getAccessToken(request));
-        String nickname = memberRequest.getNickname();
+    public void updateMember(@RequestBody MemberRequest memberRequest,
+                             @RequestHeader("Authorization") String authorizationHeader) {
 
-        log.info("[POST /member] email: {}, nickname: {}", email, nickname);
+        // todo: 예외처리 추가하기
+//        if (authorizationHeader == null || authorizationHeader.startsWith("Bearer") {
+//
+//        }
 
-        Optional<Member> optionalMember = Optional.ofNullable(repository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다.")));
+        StringTokenizer tokenizer = new StringTokenizer(authorizationHeader);
+        if (tokenizer.countTokens() == 2 && tokenizer.nextToken().equals("Bearer")) {
+            String token = tokenizer.nextToken();
+            log.info("[POST /member] token: {}", token);
+            String email = tokenProvider.getEmail(token);
+            String nickname = memberRequest.getNickname();
 
-        log.info("[POST /member] 회원 조회 성공");
+            log.info("[POST /member] email: {}, nickname: {}", email, nickname);
 
-        Member member = optionalMember.get();
-        member.activate(nickname);
-        repository.save(member);
+            Member member = memberService.findByEmail(email);
 
-        log.info("[POST /member] 회원 활성화 성공");
+            log.info("[POST /member] 회원 조회 성공");
+
+            member.activate(nickname);
+            memberService.save(member);
+
+            log.info("[POST /member] 회원 활성화 성공");
+        }
     }
 }
