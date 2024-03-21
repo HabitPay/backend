d1744cdd32707d4868b3ffcc5ffddfafddafdd61
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-03-21 23:24:51 +0900

    :sparkles: feat: Add issuing newly generated accessToken to the client #14

--- src/main/java/com/habitpay/habitpay/domain/member/api/MemberApi.java
+++ src/main/java/com/habitpay/habitpay/domain/member/api/MemberApi.java
@@ -1,12 +1,14 @@
 package com.habitpay.habitpay.domain.member.api;
 
+import com.google.gson.JsonObject;
 import com.habitpay.habitpay.domain.member.application.MemberService;
 import com.habitpay.habitpay.domain.member.domain.Member;
 import com.habitpay.habitpay.domain.member.dto.MemberRequest;
-import com.habitpay.habitpay.global.config.jwt.TokenProvider;
+import com.habitpay.habitpay.global.config.jwt.TokenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.StringTokenizer;
@@ -16,12 +18,12 @@ import java.util.StringTokenizer;
 @Slf4j
 public class MemberApi {
     private final MemberService memberService;
-    private final TokenProvider tokenProvider;
+    private final TokenService tokenService;
 
     @PostMapping("/member")
     @ResponseStatus(HttpStatus.CREATED)
-    public void updateMember(@RequestBody MemberRequest memberRequest,
-                             @RequestHeader("Authorization") String authorizationHeader) {
+    public ResponseEntity<String> updateMember(@RequestBody MemberRequest memberRequest,
+                                               @RequestHeader("Authorization") String authorizationHeader) {
 
         // todo: 예외처리 추가하기
 //        if (authorizationHeader == null || authorizationHeader.startsWith("Bearer") {
@@ -32,7 +34,7 @@ public class MemberApi {
         if (tokenizer.countTokens() == 2 && tokenizer.nextToken().equals("Bearer")) {
             String token = tokenizer.nextToken();
             log.info("[POST /member] token: {}", token);
-            String email = tokenProvider.getEmail(token);
+            String email = tokenService.getEmail(token);
             String nickname = memberRequest.getNickname();
 
             log.info("[POST /member] email: {}, nickname: {}", email, nickname);
@@ -45,6 +47,14 @@ public class MemberApi {
             memberService.save(member);
 
             log.info("[POST /member] 회원 활성화 성공");
+
+            String newToken = tokenService.createAccessToken(email);
+            JsonObject responseBody = new JsonObject();
+            responseBody.addProperty("accessToken", newToken);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(responseBody.toString());
         }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("올바른 접근이 아닙니다.");
     }
 }
