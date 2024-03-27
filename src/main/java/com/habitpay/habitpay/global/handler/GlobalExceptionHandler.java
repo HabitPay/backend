f554c16b2f9a3039a7ba47bc1547595164deded0
ssolpang <iless3377@naver.com>
2024-03-27 18:30:29 +0900

    test/@RestControllerAdvice_exception_test#16

--- src/main/java/com/habitpay/habitpay/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/habitpay/habitpay/global/handler/GlobalExceptionHandler.java
@@ -1,4 +1,15 @@
 package com.habitpay.habitpay.global.handler;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalAccessException.class)
+    protected String illegalAccessExceptionHandler(IllegalAccessException exception) {
+        return "@RestControllerAdvice GlobalExceptionHandler IllegalAccessException let's go";
+    }
 }
