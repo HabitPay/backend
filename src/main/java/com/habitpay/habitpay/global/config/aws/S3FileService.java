25a44683d4818ce83326c7baef7d4c410eacbf94
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-03-29 02:53:09 +0900

    :sparkles: feat: Add getting aws s3 presigned url in S3FileService.java #17

---
+++ src/main/java/com/habitpay/habitpay/global/config/aws/S3FileService.java
@@ -0,0 +1,38 @@
+package com.habitpay.habitpay.global.config.aws;
+
+import io.awspring.cloud.s3.S3Operations;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class S3FileService {
+    private final S3Operations amazonS3;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String getPreSignedUrl(String prefix, String fileName) {
+        String filePath = String.format("%s/%s", prefix, fileName);
+        try (S3Presigner presigner = S3Presigner.create()) {
+            PutObjectRequest objectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(filePath)
+                    .build();
+
+            PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                    .signatureDuration(Duration.ofMinutes(2))
+                    .putObjectRequest(objectRequest)
+                    .build();
+
+            PresignedPutObjectRequest presignedRequest = presigner.presignPutObject(presignRequest);
+            return presignedRequest.url().toString();
+        }
+    }
+}
