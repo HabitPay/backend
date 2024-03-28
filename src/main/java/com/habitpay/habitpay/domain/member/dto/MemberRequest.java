efa450fdbd463f1d02aefed30a804f3cd81bee79
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-03-28 21:48:18 +0900

    :sparkles: feat: Add MultipartFile type variable and Setter for saving uploaded profile image #17

--- src/main/java/com/habitpay/habitpay/domain/member/dto/MemberRequest.java
+++ src/main/java/com/habitpay/habitpay/domain/member/dto/MemberRequest.java
@@ -1,9 +1,12 @@
 package com.habitpay.habitpay.domain.member.dto;
 
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+@Setter
 public class MemberRequest {
     private String nickname;
-    private String email;
+    private MultipartFile profileImage;
 }
