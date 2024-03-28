c6cc46a8958010a1946a62181ca2b857b97d7d35
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-03-29 02:53:46 +0900

    :sparkles: feat: Replace profile image file with profile image name #17
    - 프론트에서 s3 presigned url 을 받아서 프론트가 직접 s3 에 업로드 하는 방식으로 변경

--- src/main/java/com/habitpay/habitpay/domain/member/dto/MemberRequest.java
+++ src/main/java/com/habitpay/habitpay/domain/member/dto/MemberRequest.java
@@ -1,12 +1,10 @@
 package com.habitpay.habitpay.domain.member.dto;
 
 import lombok.Getter;
-import lombok.Setter;
-import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@Setter
+//@Setter
 public class MemberRequest {
     private String nickname;
-    private MultipartFile profileImage;
+    private String profileImageName;
 }
