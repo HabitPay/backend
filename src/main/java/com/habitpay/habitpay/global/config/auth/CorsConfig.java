fcdce79cf35c96eff8e7638974aaecdfd552ab17
ssolpang <iless3377@naver.com>
2024-04-01 23:00:44 +0900

    feat/addInterceptor_config_to_CorsConfig_class#18

--- src/main/java/com/habitpay/habitpay/global/config/auth/CorsConfig.java
+++ src/main/java/com/habitpay/habitpay/global/config/auth/CorsConfig.java
@@ -1,15 +1,38 @@
-//package com.habitpay.habitpay.config.auth;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedMethods("*")
-//                .allowedOrigins("http://localhost:3000");
-//    }
-//}
+package com.habitpay.habitpay.global.config.auth;
+
+import com.habitpay.habitpay.global.config.jwt.TokenProvider;
+import com.habitpay.habitpay.global.config.jwt.TokenService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    private final TokenService tokenService;
+    private final TokenProvider tokenProvider;
+
+    public CorsConfig(TokenService tokenService, TokenProvider tokenProvider){
+        this.tokenService = tokenService;
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("http://localhost:3000");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor());
+    }
+
+    @Bean
+    public AuthorizationInterceptor authorizationInterceptor() {
+        return new AuthorizationInterceptor(tokenService, tokenProvider);
+    }
+}
