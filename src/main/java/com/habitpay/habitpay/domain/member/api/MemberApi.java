13c1a04805a4e9bda792945b672d3bbfeabc7a51
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-04-04 15:25:31 +0900

    :construction: feat: Add generating random file name by using uuid #17
    - 이미지 파일 이름의 중복을 막기 위해 uuid 사용.

--- src/main/java/com/habitpay/habitpay/domain/member/api/MemberApi.java
+++ src/main/java/com/habitpay/habitpay/domain/member/api/MemberApi.java
@@ -6,13 +6,16 @@ import com.habitpay.habitpay.domain.member.domain.Member;
 import com.habitpay.habitpay.domain.member.dto.MemberRequest;
 import com.habitpay.habitpay.global.config.aws.S3FileService;
 import com.habitpay.habitpay.global.config.jwt.TokenService;
+import com.habitpay.habitpay.global.error.ErrorResponse;
+import com.habitpay.habitpay.global.util.ImageUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.StringTokenizer;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,49 +30,61 @@ public class MemberApi {
     public ResponseEntity<String> activateMember(@RequestBody MemberRequest memberRequest,
                                                  @RequestHeader("Authorization") String authorizationHeader) {
 
-        // todo: 예외처리 추가하기
-//        if (authorizationHeader == null || authorizationHeader.startsWith("Bearer") {
-//
-//        }
-
-        StringTokenizer tokenizer = new StringTokenizer(authorizationHeader);
-        if (tokenizer.countTokens() == 2 && tokenizer.nextToken().equals("Bearer")) {
-            String token = tokenizer.nextToken();
-            log.info("[POST /member] token: {}", token);
-            String email = tokenService.getEmail(token);
-            String nickname = memberRequest.getNickname();
-
-            log.info("[POST /member] email: {}, nickname: {}", email, nickname);
-
-            Member member = memberService.findByEmail(email);
-
-            log.info("[POST /member] 회원 조회 성공");
-
-            member.activate(nickname);
-            memberService.save(member);
-
-            log.info("[POST /member] 회원 활성화 성공");
-
-            String newToken = tokenService.createAccessToken(email);
-            JsonObject responseBody = new JsonObject();
-            responseBody.addProperty("accessToken", newToken);
-
-            return ResponseEntity.status(HttpStatus.CREATED).body(responseBody.toString());
+        // TODO: Interceptor 나 Filter 에서 먼저 처리해주기 때문에 나중에 삭제하기
+        Optional<String> optionalToken = tokenService.getTokenFromHeader(authorizationHeader);
+        if (optionalToken.isEmpty()) {
+            String message = ErrorResponse.UNAUTHORIZED.getMessage();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(message);
         }
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("올바른 접근이 아닙니다.");
+        String token = optionalToken.get();
+        log.info("[POST /member] token: {}", token);
+
+        String email = tokenService.getEmail(token);
+        String nickname = memberRequest.getNickname();
+        log.info("[POST /member] email: {}, nickname: {}", email, nickname);
+
+        Member member = memberService.findByEmail(email);
+
+        log.info("[POST /member] 회원 조회 성공");
+
+        member.activate(nickname);
+        memberService.save(member);
+
+        log.info("[POST /member] 회원 활성화 성공");
+
+        String newToken = tokenService.createAccessToken(email);
+        JsonObject responseBody = new JsonObject();
+        responseBody.addProperty("accessToken", newToken);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseBody.toString());
+
     }
 
     @PatchMapping("/member")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<String> patchMember(@RequestBody MemberRequest memberRequest) {
-        log.info("[PATCH /member] nickname: {}", memberRequest.getNickname());
-        if (memberRequest.getProfileImageName() != null) {
-            String fileName = memberRequest.getProfileImageName();
-            log.info("[PATCH /member] fileName: {}", fileName);
-            String preSignedUrl = s3FileService.getPreSignedUrl("profiles", fileName);
-            return ResponseEntity.status(HttpStatus.OK).body(preSignedUrl);
+    public ResponseEntity<String> patchMember(@RequestBody MemberRequest memberRequest,
+                                              @RequestHeader("Authorization") String authorizationHeader) {
+        // TODO: Interceptor 나 Filter 에서 먼저 처리해주기 때문에 나중에 삭제하기
+        Optional<String> optionalToken = tokenService.getTokenFromHeader(authorizationHeader);
+        if (optionalToken.isEmpty()) {
+            String message = ErrorResponse.UNAUTHORIZED.getMessage();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(message);
         }
-        return ResponseEntity.status(HttpStatus.OK).body("OK");
+
+        String imageExtension = memberRequest.getImageExtension();
+        log.info("[PATCH /member] imageExtension: {}", imageExtension);
+        if (ImageUtil.isValidImageExtension(imageExtension) == false) {
+            String message = ErrorResponse.UNSUPPORTED_IMAGE_EXTENSION.getMessage();
+            return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(message);
+        }
+
+        log.info("[PATCH /member] nickname: {}", memberRequest.getNickname());
+
+        String randomFileName = UUID.randomUUID().toString();
+        log.info("[PATCH /member] randomFileName: {}", randomFileName);
+        String preSignedUrl = s3FileService.getPreSignedUrl("profiles", randomFileName);
+        return ResponseEntity.status(HttpStatus.OK).body(preSignedUrl);
     }
+
 }
