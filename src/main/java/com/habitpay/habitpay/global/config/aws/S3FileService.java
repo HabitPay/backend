2be260f145c1d0e4e0d6a06f86f4a90fa44f6c4f
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-04-05 00:28:50 +0900

    :sparkles: feat: Add s3 GET presigned url method #17

--- src/main/java/com/habitpay/habitpay/global/config/aws/S3FileService.java
+++ src/main/java/com/habitpay/habitpay/global/config/aws/S3FileService.java
@@ -2,16 +2,21 @@ package com.habitpay.habitpay.global.config.aws;
 
 import io.awspring.cloud.s3.S3Operations;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
 import java.time.Duration;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class S3FileService {
     private final S3Operations amazonS3;
@@ -32,7 +37,27 @@ public class S3FileService {
                     .build();
 
             PresignedPutObjectRequest presignedRequest = presigner.presignPutObject(presignRequest);
+            log.info("[S3FileService] getPutPreSignedUrl: {}", presignedRequest.url().toString());
             return presignedRequest.url().toString();
+        }
+    }
+
+    public String getGetPreSignedUrl(String prefix, String fileName) {
+        String filePath = String.format("%s/%s", prefix, fileName);
+        try (S3Presigner presigner = S3Presigner.create()) {
+            GetObjectRequest objectRequest = GetObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(filePath)
+                    .build();
+
+            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                    .signatureDuration(Duration.ofMinutes(2))
+                    .getObjectRequest(objectRequest)
+                    .build();
+
+            PresignedGetObjectRequest presignedRequest = presigner.presignGetObject(presignRequest);
+            log.info("[S3FileService] getGetPreSignedUrl: {}", presignedRequest.url().toString());
+            return presignedRequest.url().toExternalForm();
         }
     }
 }
