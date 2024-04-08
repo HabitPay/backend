672f24425acb5252179215dbc2b871dc25bd07a8
ssolpang <iless3377@naver.com>
2024-04-09 01:28:59 +0900

    feat/refreshToken_package#20

---
+++ src/main/java/com/habitpay/habitpay/domain/refreshToken/domain/RefreshToken.java
@@ -0,0 +1,42 @@
+package com.habitpay.habitpay.domain.refreshToken.domain;
+
+import com.habitpay.habitpay.domain.model.BaseTime;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "refresh_token")
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // todo: 외래키 연결
+    @Column(name = "user_id", nullable = false, unique = true)
+    private Long userId;
+
+    @Column(name = "refresh_token", nullable = false)
+    private String refreshToken;
+
+    // todo: 과정 좀 복잡하면 즉시 삭제
+    @Column(name = "login_ip", nullable = false)
+    private String loginIp;
+
+    public RefreshToken(Long userId, String refreshToken, String loginIp) {
+        this.userId = userId;
+        this.refreshToken = refreshToken;
+        this.loginIp = loginIp;
+    }
+
+    public RefreshToken update(String newRefreshToken, String newLoginIp) {
+        this.refreshToken = newRefreshToken;
+        this.loginIp = newLoginIp;
+
+        return this;
+    }
+}
