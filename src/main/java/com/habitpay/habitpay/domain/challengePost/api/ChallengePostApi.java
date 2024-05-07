07cafd9ef623265df0a359614cab5fb1e19a2b1f
ssolpang <iless3377@naver.com>
2024-05-07 20:11:39 +0900

    feat/make_java_class_file_for_challengePost_service_&_api#31

---
+++ src/main/java/com/habitpay/habitpay/domain/challengePost/api/ChallengePostApi.java
@@ -0,0 +1,11 @@
+package com.habitpay.habitpay.domain.challengePost.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class ChallengePostApi {
+}
---
+++ src/main/java/com/habitpay/habitpay/domain/challengePost/application/ChallengePostService.java
@@ -0,0 +1,9 @@
+package com.habitpay.habitpay.domain.challengePost.application;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ChallengePostService {
+}
