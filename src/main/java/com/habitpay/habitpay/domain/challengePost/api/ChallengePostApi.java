0e13de4e6eb61742148514646a4cdd2bff90ee1a
ssolpang <iless3377@naver.com>
2024-05-24 03:01:40 +0900

    feat/ChallengePost:findByChallengeEnrollmentId_method#46

--- src/main/java/com/habitpay/habitpay/domain/challengePost/api/ChallengePostApi.java
+++ src/main/java/com/habitpay/habitpay/domain/challengePost/api/ChallengePostApi.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,31 +32,14 @@ public class ChallengePostApi {
     @GetMapping("/posts/{id}")
     //    @GetMapping("/api/posts/{id}")
     public ResponseEntity<PostViewResponse> findPost(@PathVariable Long id) {
+
         ChallengePost challengePost = challengePostService.findById(id);
         List<PostPhoto> photoList = postPhotoService.findAllByPost(id);
-
-        List<PostPhotoView> photoViewList = new ArrayList<>();
-
-        for (PostPhoto photo : photoList) {
-            photoViewList.add(new PostPhotoView(photo.getViewOrder(), postPhotoService.getImageUrl(photo)));
-        }
+        List<PostPhotoView> photoViewList = postPhotoService.makePhotoViewList(photoList);
 
         return ResponseEntity.ok()
                 .body(new PostViewResponse(challengePost, photoViewList));
     }
-    // todo : test 용도 (auth 필요 없는 컨트롤러)
-//    @GetMapping("/posts/{id}")
-//    public ResponseEntity<String> findPost(@PathVariable("id")String id) {
-//        return ResponseEntity.ok()
-//                .body("Post id : " + id);
-//    }
-//    @GetMapping("/posts")
-//    public ResponseEntity<PostViewResponse> findPost(@RequestParam(value = "id")Long id) {
-//        ChallengePost challengePost = challengePostService.findById(id);
-//
-//        return ResponseEntity.ok()
-//                .body(new PostViewResponse(challengePost));
-//    }
 
     // todo : 한 챌린지 내의 모든 포스트 보기
 //    @GetMapping("/api/challenge/{id}/posts")
@@ -70,43 +54,31 @@ public class ChallengePostApi {
 //    }
 
     // todo : 한 챌린지 내에서 내가 등록한 포스트만 모아보기
+    @GetMapping("/challenge_enrollment/{id}/posts")
 //    @GetMapping("/api/challenge_enrollment/{id}/posts")
-//    public ResponseEntity<List<PostViewResponse>> findChallengeEnrollmentPosts(@PathVariable Long id) {
-//        List<PostViewResponse> posts = challengePostService.findAllByChallengeEnrollment(id)
-//                .stream()
-//                .map(PostViewResponse::new)
-//                .toList();
-//
-//        return ResponseEntity.ok()
-//                .body(posts);
-//    }
+    public ResponseEntity<List<PostViewResponse>> findChallengeEnrollmentPosts(@PathVariable Long id) {
+        List<PostViewResponse> posts = challengePostService.findAllByChallengeEnrollment(id)
+                .stream()
+                // .filter() // todo : isAnnouncement == true 인 건 제외하기
+                .map(post -> new PostViewResponse(post, postPhotoService.makePhotoViewList(postPhotoService.findAllByPost(post.getId()))))
+                .toList();
 
-//    @PostMapping("/api/challenge_enrollment/{id}/post")
-//    public ResponseEntity<ChallengePost> addPost(@RequestBody AddPostRequest request, @PathVariable Long id, Principal principal) {
-//        ChallengePost challengePost = challengePostService.save(request, id);
-//        // todo : principal.getName() 정보 확인 필요 없으면 인자 지우기
-//        System.out.println("Principal: " + principal.getName());
-//
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(challengePost);
-//    }
+        return ResponseEntity.ok()
+                .body(posts);
+    }
 
-    // todo : 인터셉터 거치치 않는 테스트 용도 컨트롤러 (/api/challenge_enrollment/{id} 경로 없앤 버전)
-    @PostMapping("/post")
-    // public ResponseEntity<ChallengePost> addPost(@RequestBody AddPostRequest request, Principal principal) {
-    public ResponseEntity<List<String>> addPost(@RequestBody AddPostRequest request, Principal principal) {
+    @PostMapping("/challenge_enrollment/{id}/post")
+    //@PostMapping("/api/challenge_enrollment/{id}/post")
+    public ResponseEntity<List<String>> addPost(@RequestBody AddPostRequest request, @PathVariable Long id, Principal principal) {
 
-            // todo : enrollmentId 만들고 난 후 수정하기
-        ChallengePost challengePost = challengePostService.save(request, 1L);
+        ChallengePost challengePost = challengePostService.save(request, id);
 
         // todo : principal.getName() 정보 확인 필요 없으면 인자 지우기
-//        System.out.println("Principal: " + principal.getName());
+        // System.out.println("Principal: " + principal.getName());
 
         List<String> preSignedUrlList = postPhotoService.save(request.getPhotos());
 
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(challengePost);
-        // todo : postPhoto 저장할 url 링크 보내주기 -> front에서 어떻게 처리되는지?
+        // todo : postPhoto 저장할 url 링크 -> front에서 어떻게 처리되는지?
         return ResponseEntity.status(HttpStatus.CREATED).body(preSignedUrlList);
     }
 
--- src/main/java/com/habitpay/habitpay/domain/challengePost/dao/ChallengePostRepository.java
+++ src/main/java/com/habitpay/habitpay/domain/challengePost/dao/ChallengePostRepository.java
@@ -2,9 +2,11 @@ package com.habitpay.habitpay.domain.challengePost.dao;
 
 import com.habitpay.habitpay.domain.challengePost.domain.ChallengePost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChallengePostRepository extends JpaRepository<ChallengePost, Long> {
-    Optional<ChallengePost> findByChallengeEnrollmentId(Long challengeEnrollmentId);
+    Optional<List<ChallengePost>> findAllByChallengeEnrollmentId(Long challengeEnrollmentId);
 }
