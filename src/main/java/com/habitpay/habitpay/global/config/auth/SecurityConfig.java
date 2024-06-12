2b00980d326fec4e86c2789d922ca13b3f9a66e9
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-06-12 10:15:22 +0900

    :sparkles: feat: JWT 토큰 필터 기능 추가 #69
    - SecurityConfig 에 JWT 필터를 안 넣어서 작동하지 않고 있었음

--- src/main/java/com/habitpay/habitpay/global/config/auth/SecurityConfig.java
+++ src/main/java/com/habitpay/habitpay/global/config/auth/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.habitpay.habitpay.global.config.auth;
 
+import com.habitpay.habitpay.global.config.jwt.TokenAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -20,6 +22,7 @@ import java.util.Collections;
 public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomOAuth2LoginSuccessHandler customOAuth2LoginSuccessHandler;
+    private final TokenAuthenticationFilter tokenAuthenticationFilter;
 
     // TODO: CorsConfig.java 파일에 옮길 수 있도록 하기 
     @Bean
@@ -57,7 +60,8 @@ public class SecurityConfig {
                                 .successHandler(customOAuth2LoginSuccessHandler)
                                 .failureUrl("http://localhost:3000/fail")
                                 .userInfoEndpoint(userInfoEndpoint ->
-                                        userInfoEndpoint.userService(customOAuth2UserService)));
+                                        userInfoEndpoint.userService(customOAuth2UserService)))
+                .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
