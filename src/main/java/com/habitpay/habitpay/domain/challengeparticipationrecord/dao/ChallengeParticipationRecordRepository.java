0bc6a7f3c0687d8384919d536d80900fecd6adef
ssolpang <iless3377@naver.com>
2024-06-13 17:57:13 +0900

    feat/change_name:Proof_to_ChallengeParticipationRecord#73

---
+++ src/main/java/com/habitpay/habitpay/domain/challengeparticipationrecord/dao/ChallengeParticipationRecordRepository.java
@@ -0,0 +1,8 @@
+package com.habitpay.habitpay.domain.challengeparticipationrecord.dao;
+
+import com.habitpay.habitpay.domain.challengeparticipationrecord.domain.ChallengeParticipationRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChallengeParticipationRecordRepository extends JpaRepository<ChallengeParticipationRecord, Long> {
+
+}
