6165d765936de373a0efd0ab9b5fdb92ed2bfc5d
ssolpang <iless3377@naver.com>
2024-06-17 18:27:27 +0900

    feat/add_comment_for_later_method#83

--- src/main/java/com/habitpay/habitpay/domain/challengeparticipationrecord/application/ChallengeParticipationRecordService.java
+++ src/main/java/com/habitpay/habitpay/domain/challengeparticipationrecord/application/ChallengeParticipationRecordService.java
@@ -17,6 +17,7 @@ public class ChallengeParticipationRecordService {
 
     public ChallengeParticipationRecord save(ChallengeEnrollment enrollment, ChallengePost post) {
         // todo : 챌린지 조건 따져서 인정될 때만 저장해야 함
+        // todo : enrollment service에서 enrollment의 성공 횟수를 +1하는 메서드 만들고, 여기에 넣어야 할 듯
         return challengeParticipationRecordRepository.save(
                 ChallengeParticipationRecord.builder()
                         .enrollment(enrollment)
