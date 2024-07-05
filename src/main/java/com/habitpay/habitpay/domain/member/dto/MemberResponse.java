98065b652020c088a974d6d258f8951130ea2bcb
joonhan <joonhan@student.42seoul.kr>
2024-07-05 18:12:47 +0900

    refactor: GET /member 테스트를 위한 서비스 로직 분리 및 리팩토링 #102

--- src/main/java/com/habitpay/habitpay/domain/member/dto/MemberResponse.java
+++ src/main/java/com/habitpay/habitpay/domain/member/dto/MemberResponse.java
@@ -1,9 +1,13 @@
 package com.habitpay.habitpay.domain.member.dto;
 
+import com.habitpay.habitpay.domain.member.domain.Member;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class MemberResponse {
     private String nickname;
