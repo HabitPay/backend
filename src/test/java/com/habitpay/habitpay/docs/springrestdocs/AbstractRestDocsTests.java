9ea538710b89827df1957539437590d1a6686224
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-07-07 23:38:50 +0900

    :recycle: refactor: 함수 이름 변경 #102

--- src/test/java/com/habitpay/habitpay/docs/springrestdocs/AbstractRestDocsTests.java
+++ src/test/java/com/habitpay/habitpay/docs/springrestdocs/AbstractRestDocsTests.java
@@ -1,12 +1,8 @@
 package com.habitpay.habitpay.docs.springrestdocs;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,16 +11,19 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
 @ExtendWith(RestDocumentationExtension.class)
 public abstract class AbstractRestDocsTests {
     @Autowired
     protected MockMvc mockMvc;
 
     @BeforeEach
-    void setUpRestDocs(
+    void setUp(
             final WebApplicationContext context,
             final RestDocumentationContextProvider restDocumentation
-            ) {
+    ) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .apply(documentationConfiguration(restDocumentation)
                         .operationPreprocessors()
