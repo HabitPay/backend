8cb155b221e58aa41b9d57b865f3713826dfc704
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-07-07 23:36:56 +0900

    :test_tube: test: 테스트 실행을 위한 클래스 지정  #102
    - Unable to find a @SpringBootConfiguration, you need to use @ContextConfiguration or @SpringBootTest(classes=...) with your test 오류 해결

--- src/test/java/com/habitpay/habitpay/HabitpayApplicationTests.java
+++ src/test/java/com/habitpay/habitpay/HabitpayApplicationTests.java
@@ -3,11 +3,10 @@ package com.habitpay.habitpay;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HabitpayApplication.class)
 class HabitpayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
