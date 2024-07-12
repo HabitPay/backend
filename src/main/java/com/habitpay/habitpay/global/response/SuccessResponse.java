7958ac804d96ef5b51001d7f990d04e91982c95b
joonhan <joonhan@student.42seoul.kr>
2024-07-12 19:25:09 +0900

    feat: SuccessResponse 에 message 필드 추가 #117

--- src/main/java/com/habitpay/habitpay/global/response/SuccessResponse.java
+++ src/main/java/com/habitpay/habitpay/global/response/SuccessResponse.java
@@ -1,10 +1,18 @@
 package com.habitpay.habitpay.global.response;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@Builder
 public class SuccessResponse<T> {
+    private final String message;
     private final T data;
+
+    public static <T> SuccessResponse<T> of(String message, T data) {
+        return SuccessResponse.<T>builder()
+                .message(message)
+                .data(data)
+                .build();
+    }
 }
