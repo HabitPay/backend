920727dbee34e5e65157148107cc5d6fa6ae9e6e
ssolpang <iless3377@naver.com>
2024-07-13 16:31:55 +0900

    feat/throw_exeption_for_FeeSearchService#126

--- src/main/java/com/habitpay/habitpay/domain/ChallengeAbsenceFee/application/ChallengeAbsenceFeeSearchService.java
+++ src/main/java/com/habitpay/habitpay/domain/ChallengeAbsenceFee/application/ChallengeAbsenceFeeSearchService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -20,8 +21,8 @@ public class ChallengeAbsenceFeeSearchService {
 
     public int findPersonalTotalFeeOfChallenge(Member member, Challenge challenge) {
         Optional<ChallengeEnrollment> optionalEnrollment = challengeEnrollmentSearchService.findByMemberAndChallenge(member, challenge);
-        // todo: enrollment 객체가 없을 때 throw 에러 처리할 것인지, 벌금으로 나올 수 없는 값인 -1 등을 반환하여 클라이언트 메서드가 알아서 처리할 것인지
-        return optionalEnrollment.map(ChallengeEnrollment::getTotalFee).orElse(-1);
+        return optionalEnrollment.map(ChallengeEnrollment::getTotalFee)
+                .orElseThrow(() -> new NoSuchElementException("This member is not in this challenge."));
     }
 
     public int findPersonalTotalFeeOfChallenge(ChallengeEnrollment enrollment) {
