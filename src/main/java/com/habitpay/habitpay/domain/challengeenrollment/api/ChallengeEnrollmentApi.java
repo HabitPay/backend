ff5eebd34f245ed7f6d2456a7312d7f749b080eb
joonhan <joonhan@student.42seoul.kr>
2024-07-18 12:21:53 +0900

    refactor: 컨트롤러 매개변수 email 에서 CustomUserDetails 로 변경 #144

--- src/main/java/com/habitpay/habitpay/domain/challengeenrollment/api/ChallengeEnrollmentApi.java
+++ src/main/java/com/habitpay/habitpay/domain/challengeenrollment/api/ChallengeEnrollmentApi.java
@@ -2,36 +2,33 @@ package com.habitpay.habitpay.domain.challengeenrollment.api;
 
 import com.habitpay.habitpay.domain.challengeenrollment.application.ChallengeCancellationService;
 import com.habitpay.habitpay.domain.challengeenrollment.application.ChallengeEnrollmentService;
-import com.habitpay.habitpay.domain.member.application.MemberService;
+import com.habitpay.habitpay.global.config.auth.CustomUserDetails;
 import com.habitpay.habitpay.global.response.ApiResponse;
-import com.habitpay.habitpay.global.config.jwt.TokenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class ChallengeEnrollmentApi {
-    private final MemberService memberService;
-    private final TokenService tokenService;
     private final ChallengeEnrollmentService challengeEnrollmentService;
     private final ChallengeCancellationService challengeCancellationService;
 
     @PostMapping("/challenges/{id}/enroll")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<ApiResponse> enrollChallenge(@PathVariable("id") Long id, @AuthenticationPrincipal String email) {
-        return challengeEnrollmentService.enroll(id, email);
+    public ResponseEntity<ApiResponse> enrollChallenge(@PathVariable("id") Long id, @AuthenticationPrincipal CustomUserDetails user) {
+        return challengeEnrollmentService.enroll(id, user.getId());
     }
 
     @PostMapping("/challenges/{id}/cancel")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<ApiResponse> cancelChallenge(@PathVariable("id") Long id, @AuthenticationPrincipal String email) {
-        return challengeCancellationService.cancel(id, email);
+    public ResponseEntity<ApiResponse> cancelChallenge(@PathVariable("id") Long id, @AuthenticationPrincipal CustomUserDetails user) {
+        return challengeCancellationService.cancel(id, user.getId());
     }
 
 }
