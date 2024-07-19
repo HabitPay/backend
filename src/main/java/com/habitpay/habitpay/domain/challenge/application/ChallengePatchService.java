ddf8903352b2b1fa33edb60deb309ea2cb98d9ae
joonhan <joonhan@student.42seoul.kr>
2024-07-19 11:22:40 +0900

    feat: 챌린지 정보 수정 컨트롤러 반환 자료형 변경 및 변경사항 없는 경우 예외처리 추가 #148

--- src/main/java/com/habitpay/habitpay/domain/challenge/application/ChallengePatchService.java
+++ src/main/java/com/habitpay/habitpay/domain/challenge/application/ChallengePatchService.java
@@ -3,42 +3,50 @@ package com.habitpay.habitpay.domain.challenge.application;
 import com.habitpay.habitpay.domain.challenge.dao.ChallengeRepository;
 import com.habitpay.habitpay.domain.challenge.domain.Challenge;
 import com.habitpay.habitpay.domain.challenge.dto.ChallengePatchRequest;
+import com.habitpay.habitpay.domain.challenge.dto.ChallengePatchResponse;
 import com.habitpay.habitpay.domain.member.application.MemberSearchService;
 import com.habitpay.habitpay.domain.member.domain.Member;
-import com.habitpay.habitpay.global.response.ApiResponse;
+import com.habitpay.habitpay.global.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class ChallengeUpdateService {
+public class ChallengePatchService {
     private final MemberSearchService memberSearchService;
     private final ChallengeRepository challengeRepository;
     private final ChallengeSearchService challengeSearchService;
 
     @Transactional
-    public ResponseEntity<ApiResponse> update(Long challengeId, ChallengePatchRequest challengePatchRequest, Long userId) {
-        ApiResponse apiResponse;
+    public SuccessResponse<ChallengePatchResponse> patch(Long challengeId, ChallengePatchRequest challengePatchRequest, Long userId) {
         Member member = memberSearchService.getMemberById(userId);
         Challenge challenge = challengeSearchService.getChallengeById(challengeId);
 
         if (isChallengeHost(member, challenge) == false) {
-            // TODO: throw 로 예외 처리 가능한지 확인하기
-            apiResponse = ApiResponse.create("챌린지 수정 권한이 없습니다.");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(apiResponse);
+            // TODO: 공통 예외 처리 응답 적용하기
+            throw new IllegalArgumentException("챌린지 주최자만 수정 가능합니다.");
         }
 
-        challenge.updateDescription(challengePatchRequest.getDescription());
+        if (isChallengeDescriptionUnchanged(challenge, challengePatchRequest)) {
+            // TODO: 공통 예외 처리 응답 적용하기
+            throw new IllegalArgumentException("변경 사항이 없습니다.");
+        }
+
+        challenge.patch(challengePatchRequest);
         challengeRepository.save(challenge);
 
-        apiResponse = ApiResponse.create("챌린지 정보 수정이 반영되었습니다.");
-        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+        return SuccessResponse.of(
+                "챌린지 정보 수정이 반영되었습니다.",
+                ChallengePatchResponse.of(challenge)
+        );
     }
 
     private boolean isChallengeHost(Member member, Challenge challenge) {
         return member.getEmail().equals(challenge.getHost().getEmail());
+    }
+
+    private boolean isChallengeDescriptionUnchanged(Challenge challenge, ChallengePatchRequest challengePatchRequest) {
+        return challenge.getDescription().equals(challengePatchRequest.getDescription());
     }
 }
