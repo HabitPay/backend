21b2bd3262d3f91f0e2674aa939090378fcc1f92
ssolpang <iless3377@naver.com>
2024-07-22 03:17:18 +0900

    feat/Refresh Token 컨트롤러의 반환 타입에 Success Response 적용, 소소한 리팩토링#153

--- src/main/java/com/habitpay/habitpay/domain/refreshtoken/api/TokenApi.java
+++ src/main/java/com/habitpay/habitpay/domain/refreshtoken/api/TokenApi.java
@@ -3,6 +3,7 @@ package com.habitpay.habitpay.domain.refreshtoken.api;
 import com.habitpay.habitpay.domain.refreshtoken.application.RefreshTokenCreationService;
 import com.habitpay.habitpay.domain.refreshtoken.dto.CreateAccessTokenRequest;
 import com.habitpay.habitpay.domain.refreshtoken.dto.CreateAccessTokenResponse;
+import com.habitpay.habitpay.global.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,10 +20,12 @@ public class TokenApi {
     private final RefreshTokenCreationService refreshTokenCreationService;
 
     @PostMapping("/token")
-    public ResponseEntity<CreateAccessTokenResponse> createNewAccessTokenAndNewRefreshToken(
+    public SuccessResponse<CreateAccessTokenResponse> createNewAccessTokenAndNewRefreshToken(
             @RequestBody CreateAccessTokenRequest requestBody) {
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(refreshTokenCreationService.createNewAccessTokenAndNewRefreshToken(requestBody));
+        return SuccessResponse.of(
+                "새로운 액세스 토큰 및 리프레시 토큰이 성공적으로 발급되었습니다.",
+                refreshTokenCreationService.createNewAccessTokenAndNewRefreshToken(requestBody)
+        );
     }
 }
