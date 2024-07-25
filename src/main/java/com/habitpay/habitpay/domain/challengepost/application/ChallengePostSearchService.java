a54651c8eaaae98a486d06501f8c50986a51b093
ssolpang <iless3377@naver.com>
2024-07-25 14:55:24 +0900

    feat/ ChallengePost 도메인에 Challenge 외래키 추가 및 관련 메서드 수정 #174

--- src/main/java/com/habitpay/habitpay/domain/challengepost/application/ChallengePostSearchService.java
+++ src/main/java/com/habitpay/habitpay/domain/challengepost/application/ChallengePostSearchService.java
@@ -97,36 +97,11 @@ public class ChallengePostSearchService {
     // todo : 각 챌린지 별로 findAll 해주는 메서드
     public List<ChallengePost> findAllByChallengeId(Long challengeId, Pageable pageable) {
         return challengePostRepository.findAllByChallengeEnrollmentId(1L, pageable); // todo : 임시값
-
-        // 방법 1 :
-        // 챌린지 아이디를 이용해 챌린지를 찾는다 (DB 검색1)
-        // 챌린지 등록 레포에서 해당 챌린지에 맞는 등록 객체 리스트를 얻는다 (DB 검색2)
-        // 각 등록 객체를 기반으로 포스트 목록을 불러온다 (DB 검색3)
-        // 이걸 또 시간이나 id 순서로 재정렬,,
-
-        // 방법 2 :
-        // 포스트 도메인에 챌린지도 외래키로 연결한다
-        // 챌린지 아이디로 모든 포스트를 찾는다! (DB 검색1)
-
-        // 방법 3 :
-        // DB View 사용하기 (잘 몰라서 학습 필요)
-
-        // 방법 1로 하려고 처음에 DB를 구성했지만, DB 검색을 3번이나 거치는 비용 + 멤버 별 포스트 목록을 각각 불러온 다음 다시 순서대로 리스트업 하는 비용이 클 듯함
-        // 특히 포스트는 아마 전체 도메인 중 가장 많은 DB 객체가 생성되는 도메인이 될 것임
-        // 조회도 자주 발생할 것이라, 많은 양을 자주 조회하는 DB가 될 것,,
-
-        // 조회 성능과 데이터 정합성 중에 선택의 기로
-
-        // 각각 만들어보고 처리 시간 비교해보고 이것저것 고민해야 할 듯 하다
     }
 
     public Challenge getChallengeByPostId(Long postId) {
         ChallengePost post = getChallengePostById(postId);
-        // todo : enrollment service에 findById() 메서드 만들기
-        ChallengeEnrollment enrollment = challengeEnrollmentRepository
-                .findById(post.getChallengeEnrollment().getId())
-                .orElseThrow(() -> new NoSuchElementException("포스트 소속 정보를 찾을 수 없습니다."));
-        return enrollment.getChallenge();
+        return post.getChallenge();
     }
 
 }
