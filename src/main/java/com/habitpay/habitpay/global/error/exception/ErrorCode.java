80046d018078f4658fcdeb243defe22ae6c15da1
joonhan <joonhan@student.42seoul.kr>
2024-07-27 12:42:19 +0900

    feat: PATCH /member/image 서비스 메서드 예외처리에 사용할 enum 추가 #193

--- src/main/java/com/habitpay/habitpay/global/error/exception/ErrorCode.java
+++ src/main/java/com/habitpay/habitpay/global/error/exception/ErrorCode.java
@@ -15,8 +15,11 @@ public enum ErrorCode {
     // Member
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     INVALID_NICKNAME_RULE(HttpStatus.BAD_REQUEST, "닉네임 규칙에 맞지 않습니다. (규칙: 길이 2~15자, 특수문자 제외)"),
-    DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "이전과 동일한 닉네임으로 변경할 수 없습니다.");
+    DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "이전과 동일한 닉네임으로 변경할 수 없습니다."),
+    PROFILE_IMAGE_SIZE_TOO_LARGE(HttpStatus.BAD_REQUEST, "이미지 파일의 크기가 제한을 초과했습니다. (최대 1MB)"),
+    UNSUPPORTED_IMAGE_EXTENSION(HttpStatus.BAD_REQUEST, "지원하지 않는 이미지 확장자입니다. (png, jpg, jpeg 만 가능)"),
 
+    ;
     private HttpStatus status;
     private final String message;
 
