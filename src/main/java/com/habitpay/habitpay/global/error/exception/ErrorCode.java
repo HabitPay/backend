c8c05f195147d241b5a52be1e499c789e07b569d
joonhan <joonhan@student.42seoul.kr>
2024-07-27 16:46:47 +0900

    feat: 챌린지 참여 관련 예외처리 enum 추가 #197

--- src/main/java/com/habitpay/habitpay/global/error/exception/ErrorCode.java
+++ src/main/java/com/habitpay/habitpay/global/error/exception/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
     // Common
     ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "Entity Not Found"),
     INVALID_VALUE(HttpStatus.BAD_REQUEST, "Invalid Input Value"),
-    FORBIDDEN(HttpStatus.BAD_REQUEST, "Not Allowed to Access or Modify"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "Bad Request"),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "Not Allowed to Access or Modify"),
+    CONFLICT(HttpStatus.CONFLICT, "Conflict"),
 
     // Member
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
@@ -24,7 +26,9 @@ public enum ErrorCode {
     CHALLENGE_NOT_FOUND(HttpStatus.NOT_FOUND, "챌린지가 존재하지 않습니다."),
     CHALLENGE_START_TIME_INVALID(HttpStatus.BAD_REQUEST, "챌린지 시작 시간은 현재 시간 이후만 가능합니다."),
     ONLY_HOST_CAN_MODIFY(HttpStatus.FORBIDDEN, "챌린지 주최자만 수정 가능합니다."),
-    DUPLICATED_CHALLENGE_DESCRIPTION(HttpStatus.BAD_REQUEST, "변경 사항이 없습니다.");
+    DUPLICATED_CHALLENGE_DESCRIPTION(HttpStatus.BAD_REQUEST, "변경 사항이 없습니다."),
+    INVALID_CHALLENGE_REGISTRATION_TIME(HttpStatus.BAD_REQUEST, "챌린지 등록 가능 시간이 아닙니다."),
+    ALREADY_ENROLLED_IN_CHALLENGE(HttpStatus.CONFLICT, "이미 참여한 챌린지입니다.");
     private HttpStatus status;
     private final String message;
 
