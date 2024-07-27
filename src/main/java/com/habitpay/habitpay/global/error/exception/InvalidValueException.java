87e8a6288919db20814c3b79cf40438bd340471b
joonhan <joonhan@student.42seoul.kr>
2024-07-27 15:58:50 +0900

    feat: InvalidValueException 생성자 추가 #195

--- src/main/java/com/habitpay/habitpay/global/error/exception/InvalidValueException.java
+++ src/main/java/com/habitpay/habitpay/global/error/exception/InvalidValueException.java
@@ -6,6 +6,10 @@ public class InvalidValueException extends BusinessException {
         super(message, ErrorCode.ENTITY_NOT_FOUND);
     }
 
+    public InvalidValueException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
     public InvalidValueException(String message, ErrorCode errorCode) {
         super(message, errorCode);
     }
