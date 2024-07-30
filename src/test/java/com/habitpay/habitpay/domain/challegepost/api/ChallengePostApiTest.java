a340c94485f90989aeeb1571920b61cc2a0c1b1c
jkwak <jkwak@student.42seoul.kr>
2024-07-30 14:22:33 +0900

    test/ 챌린지 포스트 테스트 코드 순서 변경 및 문서 수정 #220

--- src/test/java/com/habitpay/habitpay/domain/challegepost/api/ChallengePostApiTest.java
+++ src/test/java/com/habitpay/habitpay/domain/challegepost/api/ChallengePostApiTest.java
@@ -179,17 +179,6 @@ public class ChallengePostApiTest extends AbstractRestDocsTests {
                                 fieldWithPath("data.content[].photoViewList[].viewOrder").description("포스트 내 포토의 순서"),
                                 fieldWithPath("data.content[].photoViewList[].imageUrl").description("포스트 포토 url"),
 
-                                fieldWithPath("data.size").description("한 페이지에 포함되는 항목의 수"),
-                                fieldWithPath("data.number").description("현재 페이지 번호"),
-                                fieldWithPath("data.sort").description("정렬 정보"),
-                                fieldWithPath("data.sort.empty").description("정렬 조건이 없는지 여부"),
-                                fieldWithPath("data.sort.unsorted").description("정렬되지 않았는지 여부"),
-                                fieldWithPath("data.sort.sorted").description("정렬되었는지 여부"),
-                                fieldWithPath("data.numberOfElements").description("현재 페이지에 있는 요소의 수"),
-                                fieldWithPath("data.first").description("이 페이지가 첫 번째 페이지인지 여부"),
-                                fieldWithPath("data.last").description("이 페이지가 마지막 페이지인지 여부"),
-                                fieldWithPath("data.empty").description("페이지가 비어있는지 여부"),
-
                                 fieldWithPath("data.pageable").description("포스트 뷰 페이지네이션 정보를 담은 Pageable 객체"),
                                 fieldWithPath("data.pageable.pageNumber").description("현재 페이지 번호"),
                                 fieldWithPath("data.pageable.pageSize").description("한 페이지에 포함되는 항목의 수"),
@@ -199,7 +188,18 @@ public class ChallengePostApiTest extends AbstractRestDocsTests {
                                 fieldWithPath("data.pageable.sort.sorted").description("정렬되었는지 여부"),
                                 fieldWithPath("data.pageable.offset").description("페이징된 항목의 시작 위치"),
                                 fieldWithPath("data.pageable.paged").description("페이징된 요청인지 여부"),
-                                fieldWithPath("data.pageable.unpaged").description("페이징되지 않은 요청인지 여부")
+                                fieldWithPath("data.pageable.unpaged").description("페이징되지 않은 요청인지 여부"),
+
+                                fieldWithPath("data.size").description("한 페이지에 포함되는 항목의 수"),
+                                fieldWithPath("data.number").description("현재 페이지 번호"),
+                                fieldWithPath("data.sort").description("정렬 정보"),
+                                fieldWithPath("data.sort.empty").description("정렬 조건이 없는지 여부"),
+                                fieldWithPath("data.sort.unsorted").description("정렬되지 않았는지 여부"),
+                                fieldWithPath("data.sort.sorted").description("정렬되었는지 여부"),
+                                fieldWithPath("data.numberOfElements").description("현재 페이지에 있는 요소의 수"),
+                                fieldWithPath("data.first").description("이 페이지가 첫 번째 페이지인지 여부"),
+                                fieldWithPath("data.last").description("이 페이지가 마지막 페이지인지 여부"),
+                                fieldWithPath("data.empty").description("페이지가 비어있는지 여부")
                         )
                 ));
     }
@@ -262,17 +262,6 @@ public class ChallengePostApiTest extends AbstractRestDocsTests {
                                 fieldWithPath("data.content[].photoViewList[].viewOrder").description("포스트 내 포토의 순서"),
                                 fieldWithPath("data.content[].photoViewList[].imageUrl").description("포스트 포토 url"),
 
-                                fieldWithPath("data.size").description("한 페이지에 포함되는 항목의 수"),
-                                fieldWithPath("data.number").description("현재 페이지 번호"),
-                                fieldWithPath("data.sort").description("정렬 정보"),
-                                fieldWithPath("data.sort.empty").description("정렬 조건이 없는지 여부"),
-                                fieldWithPath("data.sort.unsorted").description("정렬되지 않았는지 여부"),
-                                fieldWithPath("data.sort.sorted").description("정렬되었는지 여부"),
-                                fieldWithPath("data.numberOfElements").description("현재 페이지에 있는 요소의 수"),
-                                fieldWithPath("data.first").description("이 페이지가 첫 번째 페이지인지 여부"),
-                                fieldWithPath("data.last").description("이 페이지가 마지막 페이지인지 여부"),
-                                fieldWithPath("data.empty").description("페이지가 비어있는지 여부"),
-
                                 fieldWithPath("data.pageable").description("포스트 뷰 페이지네이션 정보를 담은 Pageable 객체"),
                                 fieldWithPath("data.pageable.pageNumber").description("현재 페이지 번호"),
                                 fieldWithPath("data.pageable.pageSize").description("한 페이지에 포함되는 항목의 수"),
@@ -282,7 +271,18 @@ public class ChallengePostApiTest extends AbstractRestDocsTests {
                                 fieldWithPath("data.pageable.sort.sorted").description("정렬되었는지 여부"),
                                 fieldWithPath("data.pageable.offset").description("페이징된 항목의 시작 위치"),
                                 fieldWithPath("data.pageable.paged").description("페이징된 요청인지 여부"),
-                                fieldWithPath("data.pageable.unpaged").description("페이징되지 않은 요청인지 여부")
+                                fieldWithPath("data.pageable.unpaged").description("페이징되지 않은 요청인지 여부"),
+
+                                fieldWithPath("data.size").description("한 페이지에 포함되는 항목의 수"),
+                                fieldWithPath("data.number").description("현재 페이지 번호"),
+                                fieldWithPath("data.sort").description("정렬 정보"),
+                                fieldWithPath("data.sort.empty").description("정렬 조건이 없는지 여부"),
+                                fieldWithPath("data.sort.unsorted").description("정렬되지 않았는지 여부"),
+                                fieldWithPath("data.sort.sorted").description("정렬되었는지 여부"),
+                                fieldWithPath("data.numberOfElements").description("현재 페이지에 있는 요소의 수"),
+                                fieldWithPath("data.first").description("이 페이지가 첫 번째 페이지인지 여부"),
+                                fieldWithPath("data.last").description("이 페이지가 마지막 페이지인지 여부"),
+                                fieldWithPath("data.empty").description("페이지가 비어있는지 여부")
                         )
                 ));
     }
@@ -346,17 +346,6 @@ public class ChallengePostApiTest extends AbstractRestDocsTests {
                                 fieldWithPath("data.content[].photoViewList[].viewOrder").description("포스트 내 포토의 순서"),
                                 fieldWithPath("data.content[].photoViewList[].imageUrl").description("포스트 포토 url"),
 
-                                fieldWithPath("data.size").description("한 페이지에 포함되는 항목의 수"),
-                                fieldWithPath("data.number").description("현재 페이지 번호"),
-                                fieldWithPath("data.sort").description("정렬 정보"),
-                                fieldWithPath("data.sort.empty").description("정렬 조건이 없는지 여부"),
-                                fieldWithPath("data.sort.unsorted").description("정렬되지 않았는지 여부"),
-                                fieldWithPath("data.sort.sorted").description("정렬되었는지 여부"),
-                                fieldWithPath("data.numberOfElements").description("현재 페이지에 있는 요소의 수"),
-                                fieldWithPath("data.first").description("이 페이지가 첫 번째 페이지인지 여부"),
-                                fieldWithPath("data.last").description("이 페이지가 마지막 페이지인지 여부"),
-                                fieldWithPath("data.empty").description("페이지가 비어있는지 여부"),
-
                                 fieldWithPath("data.pageable").description("포스트 뷰 페이지네이션 정보를 담은 Pageable 객체"),
                                 fieldWithPath("data.pageable.pageNumber").description("현재 페이지 번호"),
                                 fieldWithPath("data.pageable.pageSize").description("한 페이지에 포함되는 항목의 수"),
@@ -366,7 +355,18 @@ public class ChallengePostApiTest extends AbstractRestDocsTests {
                                 fieldWithPath("data.pageable.sort.sorted").description("정렬되었는지 여부"),
                                 fieldWithPath("data.pageable.offset").description("페이징된 항목의 시작 위치"),
                                 fieldWithPath("data.pageable.paged").description("페이징된 요청인지 여부"),
-                                fieldWithPath("data.pageable.unpaged").description("페이징되지 않은 요청인지 여부")
+                                fieldWithPath("data.pageable.unpaged").description("페이징되지 않은 요청인지 여부"),
+
+                                fieldWithPath("data.size").description("한 페이지에 포함되는 항목의 수"),
+                                fieldWithPath("data.number").description("현재 페이지 번호"),
+                                fieldWithPath("data.sort").description("정렬 정보"),
+                                fieldWithPath("data.sort.empty").description("정렬 조건이 없는지 여부"),
+                                fieldWithPath("data.sort.unsorted").description("정렬되지 않았는지 여부"),
+                                fieldWithPath("data.sort.sorted").description("정렬되었는지 여부"),
+                                fieldWithPath("data.numberOfElements").description("현재 페이지에 있는 요소의 수"),
+                                fieldWithPath("data.first").description("이 페이지가 첫 번째 페이지인지 여부"),
+                                fieldWithPath("data.last").description("이 페이지가 마지막 페이지인지 여부"),
+                                fieldWithPath("data.empty").description("페이지가 비어있는지 여부")
                         )
                 ));
     }
