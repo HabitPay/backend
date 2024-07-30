7cb85fc006c42fb4f9aab697d6e1f1fb0037810a
joonhan <joonhan@student.42seoul.kr>
2024-07-30 15:09:39 +0900

    chore: entrypoint 배포용 스크립트 추가 #217

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 
-status_output=$(./gradlew --status)
-pid=$(echo "$status_output" | grep "BUSY" | awk '{print $1}')
+echo "[Current Mode]: $1"
 
-if [ -n "$pid" ]; then
-    echo "Gradle Daemon (PID: $pid) is busy, stopping..."
-    kill -9 $pid
-    sleep 1  # Daemon이 완전히 종료되기를 기다립니다.
+development() {
+    status_output=$(./gradlew --status)
+    pid=$(echo "$status_output" | grep "BUSY" | awk '{print $1}')
+
+    if [ -n "$pid" ]; then
+        echo "Gradle Daemon (PID: $pid) is busy, stopping..."
+        kill -9 $pid
+        sleep 1  # Daemon이 완전히 종료되기를 기다립니다.
+    else
+        echo "No active Gradle Daemon found."
+    fi
+
+    ./gradlew --status
+
+    ./gradlew --build-cache --parallel -t bootRun
+}
+
+deploy() {
+    local JAR_FILE=build/libs/*.jar
+    mv $JAR_FILE app.jar
+    java -jar Dspring.profiles.active=prod app.jar
+}
+
+if [ $1 = "deploy" ]; then
+    deploy
 else
-    echo "No active Gradle Daemon found."
+    development
 fi
-
-./gradlew --status
-
-./gradlew --build-cache --parallel -t bootRun
