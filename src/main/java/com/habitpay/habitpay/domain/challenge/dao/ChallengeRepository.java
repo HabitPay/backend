36508bd3070e53d7a2761d55600235cb638cdde3
ssolpang <iless3377@naver.com>
2024-08-08 21:22:52 +0900

    feat/ Challenge 레포지토리 메서드 추가 #230

--- src/main/java/com/habitpay/habitpay/domain/challenge/dao/ChallengeRepository.java
+++ src/main/java/com/habitpay/habitpay/domain/challenge/dao/ChallengeRepository.java
@@ -3,6 +3,9 @@ package com.habitpay.habitpay.domain.challenge.dao;
 import com.habitpay.habitpay.domain.challenge.domain.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
-    
+    List<Challenge> findAllByStateAndParticipatingDays(byte state, byte participationDays);
 }
