b6b59a6b29c813cc4d7d21a32b1b43a0d1e315ae
ssolpang <iless3377@naver.com>
2024-08-11 18:53:21 +0900

    remove/ 주석 삭제 #238

--- src/main/java/com/habitpay/habitpay/domain/challengescheduler/application/ChallengeSchedulerService.java
+++ src/main/java/com/habitpay/habitpay/domain/challengescheduler/application/ChallengeSchedulerService.java
@@ -22,15 +22,15 @@ public class ChallengeSchedulerService {
     private final ChallengeRepository challengeRepository;
     private final ChallengeEnrollmentRepository challengeEnrollmentRepository;
 
-    // todo: 챌린지 시작, 종료 시 state 값 변경하는 메서드 (ScheduledExecutorService?)
+    // todo: 챌린지 시작, 종료 시 state 값 변경하는 메서드
     //     : 사용자에게 알림 보내는 로직 추가할 수 있음
 //    public void startChallenge() {}
 //    public void endChallenge() {}
 
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     public void checkParticipationForChallenge() {
-        DayOfWeek today = ZonedDateTime.now().getDayOfWeek();
-        byte todayBitPosition = (byte) ((byte) 1 << (7 - today.getValue()));
+        DayOfWeek yesterdayOfWeek = ZonedDateTime.now().minusDays(1).getDayOfWeek();
+        byte todayBitPosition = (byte) ((byte) 1 << (7 - yesterdayOfWeek.getValue()));
 
         List<Challenge> challengeList = challengeRepository.findAllByStateAndParticipatingDays(
                 ChallengeState.IN_PROGRESS.getBitValue(), todayBitPosition);
@@ -43,28 +43,4 @@ public class ChallengeSchedulerService {
                                 enrollment.resetIsParticipatedToday();
                     });
     }
-
-    // 1번 방식
-
-// find all challenge by state=진행 중 and participation days=오늘
-// find all enrollment by challenge
-// find record by enrollment and 오늘 날짜
-
-// 있으면 패스, 없으면 enrollment.failureCount +1 증가
-
-//--------------------------------
-
-// 2번 방식
-// find all challenge by state=진행 중 and participation days=오늘
-// find all enrollment by challenge -> isParticipatedToday 속성 추가,,,
-//                                     (포스트 등록해서 SuccessCount 증가 시킬 때 true로 바꾸기)
-//                                     (그리고 이 메서드 완료 후에 false로 다시 돌려놓기)
-
-// true면 패스, false면 enrollment.failureCount +1 증가
-
-// --------------------------
-
-// private method : enrollment.failureCount +1
-//                  enrollment.totalFee ++
-//                  challenge.totalAbsenceFee ++
 }
