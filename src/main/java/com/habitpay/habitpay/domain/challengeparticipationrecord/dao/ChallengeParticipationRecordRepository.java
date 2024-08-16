84e4f6c36b03313f0bf618a35937979745ef8ee2
ssolpang <iless3377@naver.com>
2024-08-16 19:02:30 +0900

    feat/ ChallengeParticipationRecordRepository에서 챌린지와 날짜 기준으로 조회하기, 근데 stat과 post 외래키를 곁들인 #240

--- src/main/java/com/habitpay/habitpay/domain/challengeparticipationrecord/dao/ChallengeParticipationRecordRepository.java
+++ src/main/java/com/habitpay/habitpay/domain/challengeparticipationrecord/dao/ChallengeParticipationRecordRepository.java
@@ -1,7 +1,10 @@
 package com.habitpay.habitpay.domain.challengeparticipationrecord.dao;
 
+import com.habitpay.habitpay.domain.challenge.domain.Challenge;
 import com.habitpay.habitpay.domain.challengeenrollment.domain.ChallengeEnrollment;
 import com.habitpay.habitpay.domain.challengeparticipationrecord.domain.ChallengeParticipationRecord;
+import org.hibernate.boot.model.source.spi.AttributePath;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -12,9 +15,14 @@ import java.util.Optional;
 public interface ChallengeParticipationRecordRepository extends JpaRepository<ChallengeParticipationRecord, Long> {
     List<ChallengeParticipationRecord> findAllByChallengeEnrollment(ChallengeEnrollment enrollment);
 
+    // todo: 사용처 보고 필요하면 수정
     Optional<ChallengeParticipationRecord> findByChallengeEnrollment(ChallengeEnrollment challengeEnrollment);
 
+    // todo: 생성 일시 말고 타겟 데이트로 찾아야 함
     Optional<ChallengeParticipationRecord> findByChallengeEnrollmentAndCreatedAtBetween(ChallengeEnrollment enrollment, LocalDateTime startOfDay, LocalDateTime endOfDay);
 
     Optional<ChallengeParticipationRecord> findByChallengeEnrollmentAndTargetDate(ChallengeEnrollment enrollment, LocalDate targetDate);
+
+    @EntityGraph(attributePaths = {"participationStat", "challengePost"})
+    List<ChallengeParticipationRecord> findByChallengeInAndTargetDate(List<Challenge> challenges, LocalDate targetDate);
 }
--- src/main/java/com/habitpay/habitpay/domain/challengescheduler/application/ChallengeSchedulerService.java
+++ src/main/java/com/habitpay/habitpay/domain/challengescheduler/application/ChallengeSchedulerService.java
@@ -1,25 +1,51 @@
 package com.habitpay.habitpay.domain.challengescheduler.application;
 
+import com.habitpay.habitpay.domain.challenge.dao.ChallengeRepository;
+import com.habitpay.habitpay.domain.challenge.domain.Challenge;
+import com.habitpay.habitpay.domain.challenge.domain.ChallengeState;
+import com.habitpay.habitpay.domain.challengeparticipationrecord.dao.ChallengeParticipationRecordRepository;
+import com.habitpay.habitpay.domain.challengeparticipationrecord.domain.ChallengeParticipationRecord;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.ZonedDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ChallengeSchedulerService {
 
+    private final ChallengeRepository challengeRepository;
+    private final ChallengeParticipationRecordRepository challengeParticipationRecordRepository;
+
     // todo : Challenge 시작, 종료 시 state 변경 등
 
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     public void checkParticipationForChallenge() {
-        DayOfWeek yesterdayOfWeek = ZonedDateTime.now().minusDays(1).getDayOfWeek();
+
+        ZonedDateTime yesterday = ZonedDateTime.now().minusDays(1);
+        DayOfWeek yesterdayOfWeek = yesterday.getDayOfWeek();
         byte yesterdayBitPosition = (byte) ((byte) 1 << (7 - yesterdayOfWeek.getValue()));
+
+        List<Challenge> challengeList = challengeRepository
+                .findAllByStateAndParticipatingDays(ChallengeState.IN_PROGRESS, yesterdayBitPosition);
+        if (challengeList.isEmpty()) {
+            return;
+        }
+
+        LocalDate targetDate = yesterday.toLocalDate();
+        List<ChallengeParticipationRecord> recordList = challengeParticipationRecordRepository
+                .findByChallengeInAndTargetDate(challengeList, targetDate);
+
+
+
 
 //        1. Challenge 목록 DB 조회(진행 중, 요일 기준)
 //        2. 1번의 목록을 기준으로 record 목록 DB 조회 (어제 날짜 기준)
+
 //        3. 2번의 목록을 순회하며 challengePost 존재 여부에 따라,
 //                record에 getParticipationStat()을 이용해 setter로 값 변경
 //        4. 값이 변경된 stat을 따로 새 list로 모아뒀다가 DB 업데이트
