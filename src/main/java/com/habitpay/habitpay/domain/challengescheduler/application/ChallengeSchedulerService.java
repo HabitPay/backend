a3628e773fe9cab5b4ca631bc341ce8fcc47507a
ssolpang <iless3377@naver.com>
2024-08-23 17:00:55 +0900

    feat/ 매일 챌린지 참여를 확인하는 스케쥴러 메서드에, 검사가 끝난 실패 record를 DB에서 삭제하기 #240

--- src/main/java/com/habitpay/habitpay/domain/challengescheduler/application/ChallengeSchedulerService.java
+++ src/main/java/com/habitpay/habitpay/domain/challengescheduler/application/ChallengeSchedulerService.java
@@ -4,11 +4,13 @@ import com.habitpay.habitpay.domain.challenge.dao.ChallengeRepository;
 import com.habitpay.habitpay.domain.challenge.domain.Challenge;
 import com.habitpay.habitpay.domain.challenge.domain.ChallengeState;
 import com.habitpay.habitpay.domain.challengeparticipationrecord.dao.ChallengeParticipationRecordRepository;
+import com.habitpay.habitpay.domain.challengeparticipationrecord.domain.ChallengeParticipationRecord;
 import com.habitpay.habitpay.domain.participationstat.dao.ParticipationStatRepository;
 import com.habitpay.habitpay.domain.participationstat.domain.ParticipationStat;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -26,6 +28,7 @@ public class ChallengeSchedulerService {
 
     // todo : Challenge 시작, 종료 시 state 변경 등 메서드
 
+    @Transactional
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     public void checkParticipationForChallenge() {
 
@@ -38,15 +41,19 @@ public class ChallengeSchedulerService {
         if (challengeList.isEmpty()) { return; }
 
         LocalDate targetDate = yesterday.toLocalDate();
-        List<ParticipationStat> statList = new ArrayList<>();
+        List<ParticipationStat> failStatList = new ArrayList<>();
+        List<ChallengeParticipationRecord> failRecordList = new ArrayList<>();
+
         challengeParticipationRecordRepository.findRecordCheckDTOByChallengeInAndTargetDate(challengeList, targetDate)
                 .forEach(recordDto -> {
                     if (!recordDto.getRecord().existChallengePost()) {
                         recordDto.getStat().setFailureCountAndTotalFee(recordDto.getFeePerAbsence());
-                        statList.add(recordDto.getStat());
+                        failStatList.add(recordDto.getStat());
+                        failRecordList.add(recordDto.getRecord());
                     }
                 });
 
-        participationStatRepository.saveAll(statList);
+        participationStatRepository.saveAll(failStatList);
+        challengeParticipationRecordRepository.deleteAll(failRecordList);
     }
 }
