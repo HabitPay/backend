f508f0dab868b8aa9c35a5627d4748439e48f710
jkwak <jkwak@student.42seoul.kr>
2024-08-30 17:38:24 +0900

    feat/ 매일 챌린지 참여를 검사하는 스케쥴러 메서드를 지연 로딩 로직에 맞게 변경 #240

--- src/main/java/com/habitpay/habitpay/domain/challengescheduler/application/ChallengeSchedulerService.java
+++ src/main/java/com/habitpay/habitpay/domain/challengescheduler/application/ChallengeSchedulerService.java
@@ -3,6 +3,7 @@ package com.habitpay.habitpay.domain.challengescheduler.application;
 import com.habitpay.habitpay.domain.challenge.dao.ChallengeRepository;
 import com.habitpay.habitpay.domain.challenge.domain.Challenge;
 import com.habitpay.habitpay.domain.challenge.domain.ChallengeState;
+import com.habitpay.habitpay.domain.challengeparticipationrecord.application.ChallengeParticipationRecordSearchService;
 import com.habitpay.habitpay.domain.challengeparticipationrecord.dao.ChallengeParticipationRecordRepository;
 import com.habitpay.habitpay.domain.challengeparticipationrecord.domain.ChallengeParticipationRecord;
 import com.habitpay.habitpay.domain.participationstat.dao.ParticipationStatRepository;
@@ -23,8 +24,9 @@ import java.util.List;
 public class ChallengeSchedulerService {
 
     private final ChallengeRepository challengeRepository;
-    private final ChallengeParticipationRecordRepository challengeParticipationRecordRepository;
     private final ParticipationStatRepository participationStatRepository;
+    private final ChallengeParticipationRecordRepository challengeParticipationRecordRepository;
+    private final ChallengeParticipationRecordSearchService challengeParticipationRecordSearchService;
 
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     public void setChallengeForStart() {
@@ -35,7 +37,7 @@ public class ChallengeSchedulerService {
         // 각 챌린지 내의 멤버 리스트 얻기
         // 멤버 리스트 순회하면서 참여 목록 한 번에 모두! 만들기
 
-        // 챌린지 생성일이 시작 요일과 동일한 경우만 참여 목록 1주일치 생성하는 예외 처리 필요
+        // 챌린지 생성일이 시작 요일과 동일한 경우만 참여 목록 따로 생성하는 예외 처리 필요
     }
 
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
@@ -48,31 +50,23 @@ public class ChallengeSchedulerService {
         ZonedDateTime yesterday = ZonedDateTime.now().minusDays(1);
         DayOfWeek yesterdayOfWeek = yesterday.getDayOfWeek();
         byte yesterdayBitPosition = (byte) ((byte) 1 << (7 - yesterdayOfWeek.getValue()));
-        // 검사하고자 하는 날의 요일 비트 포지션을 구한다
 
         List<Challenge> challengeList = challengeRepository
                 .findAllByStateAndParticipatingDays(ChallengeState.IN_PROGRESS, yesterdayBitPosition);
         if (challengeList.isEmpty()) { return; }
-        // 진행 중이면서 위에서 구한 비트 포지션에 부합하는 챌린지 리스트를 조회한다
 
         LocalDate targetDate = yesterday.toLocalDate();
         List<ParticipationStat> failStatList = new ArrayList<>();
         List<ChallengeParticipationRecord> failRecordList = new ArrayList<>();
-        // 이제 레코드를 찾아야 하니까 검사하려는 날의 날짜를 다시 제대로 구한다
 
-        // 챌린지와 레코드 날짜를 기준으로 레코드 리스트를 얻어야 한다
-        // 레코드마다 챌린지 포스트 속성이 있는지 검사한다
-        // 없을 때만 매번 스탯 불러와서 수정한다 -> 이건 모아서 다시 DB에 저장도 해야 한다
-        // 레코드 삭제할 거니까 없는 레코드도 따로 저장해야 한다
-
-//        challengeParticipationRecordRepository.findRecordCheckDTOByChallengeInAndTargetDate(challengeList, targetDate)
-//                .forEach(recordDto -> {
-//                    if (!recordDto.getRecord().existChallengePost()) {
-//                        recordDto.getStat().setFailureCountAndTotalFee(recordDto.getFeePerAbsence());
-//                        failStatList.add(recordDto.getStat());
-//                        failRecordList.add(recordDto.getRecord());
-//                    }
-//                });
+        challengeParticipationRecordSearchService.findByChallengesAndTargetDate(challengeList, targetDate)
+                .forEach(record -> {
+                    if (!record.existChallengePost()) {
+                        record.getParticipationStat().setFailureCountAndTotalFee(record.getChallenge().getFeePerAbsence());
+                        failStatList.add(record.getParticipationStat());
+                        failRecordList.add(record);
+                    }
+                });
 
         participationStatRepository.saveAll(failStatList);
         challengeParticipationRecordRepository.deleteAll(failRecordList);
