de5e2e3011ddca1c55878defe84fcbd1f6621b9f
joonhan <joonhan@student.42seoul.kr>
2024-09-05 15:26:57 +0900

    chore: env 서브 모듈 pull 추가 #217

--- git-pull.sh
+++ git-pull.sh
@@ -5,6 +5,9 @@ LOG_FILE="/var/log/habitpay/deploy.log"
 main() {
     sudo -u ec2-user git checkout -- docker-compose.yaml
     sudo -u ec2-user git pull origin main
+
+    sudo -u ec2-user git -C env checkout main
+    sudo -u ec2-user git -C env pull origin main
 }
 
 main >> "$LOG_FILE" 2>&1
