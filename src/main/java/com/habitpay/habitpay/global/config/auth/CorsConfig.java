991cbbd9bf9261872d2937ee76424b3487e25f0d
joonhan <joonhan@student.42seoul.kr>
2024-09-05 19:02:37 +0900

    chore: 임시 주석 처리 #217

--- src/main/java/com/habitpay/habitpay/global/config/auth/CorsConfig.java
+++ src/main/java/com/habitpay/habitpay/global/config/auth/CorsConfig.java
@@ -1,17 +1,18 @@
 package com.habitpay.habitpay.global.config.auth;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("*")
-                .allowedOrigins("https://habitpay.link");
-    }
-
-}
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedMethods("*")
+//                .allowedOrigins("https://habitpay.link");
+//    }
+//
+//}
