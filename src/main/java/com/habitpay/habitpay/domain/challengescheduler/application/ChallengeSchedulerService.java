29c26a3f16be1be1fd0a44904464aaaf7cdb35e1
ssolpang <iless3377@naver.com>
2024-09-13 00:18:05 +0900

    feat/ 챌린지 참여 요일에 따른, 모든 참여 날짜 구하는 도메인 메서드 생성 #247

--- src/main/java/com/habitpay/habitpay/domain/challengescheduler/application/ChallengeSchedulerService.java
+++ src/main/java/com/habitpay/habitpay/domain/challengescheduler/application/ChallengeSchedulerService.java
@@ -35,24 +35,51 @@ public class ChallengeSchedulerService {
 
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     public void setChallengeForStart() {
-        // 오늘 날짜 시작인 챌린지 리스트 얻기
+//        // 오늘 날짜 시작인 챌린지 리스트 얻기
+//        // todo : 시작 날짜 ZonedDateTime으로 받는 거 데이터 타입 확인하기
+////        List<Challenge> challengeList = ;
+//        List<Challenge> challengeList = new ArrayList<>();
+//
+//        // 챌린지 리스트를 state 진행 중으로 바꾸기
+//        challengeList = challengeList
+//                .stream()
+//                .map(Challenge::setStateInProgress)
+//                .toList();
+//
+//        // 각 챌린지 내의 멤버(등록) 리스트 얻기
+//        List<ChallengeEnrollment> enrollmentList = challengeEnrollmentRepository
+//                .findAllByChallengeIn(challengeList);
+//
+//        // todo : 멤버 리스트 순회하면서 참여 목록 한 번에 모두! 만들기
+//        // 등록 객체 -> 챌린지 통해 필요한 날짜 구하기 -> 날짜 넣어서 record 만들기(외래키 연결과 함께)
+//        // 모든 등록 객체를 이렇게 순회? 그러면 챌린지 필요 날짜 구하기 작업이 불필요하게 반복되는 듯함
+
+        // -------------------------------------
+
+        // 오늘 날짜 시작 && scheduled 챌린지 리스트 얻기
         // todo : 시작 날짜 ZonedDateTime으로 받는 거 데이터 타입 확인하기
 //        List<Challenge> challengeList = ;
         List<Challenge> challengeList = new ArrayList<>();
 
-        // 챌린지 리스트를 state 진행 중으로 바꾸기
         challengeList = challengeList
                 .stream()
                 .map(Challenge::setStateInProgress)
                 .toList();
 
-        // 각 챌린지 내의 멤버 리스트 얻기
-        List<ChallengeEnrollment> enrollmentList = challengeEnrollmentRepository
-                .findAllByChallengeIn(challengeList);
+        // 챌린지 별로 순회
+        challengeList.forEach(challenge -> {
+            // 시작 날짜부터 종료 날짜까지 && 참여 요일
+        });
+        // 챌린지 참여 날짜 구하기
+        // 챌린지 참여 날짜 순회 -> 챌린지에 해당하는 등록 리스트를 각각 순회시켜서 recrod 생성
 
-        // 멤버 리스트 순회하면서 참여 목록 한 번에 모두! 만들기
+        // 챌린지 상태가 변경되었으므로 챌린지를 레포에 저장하기
+        // 새로 생성된 record 리스트들 저장하기
 
+        // -------------------------------------
         // 챌린지 생성일이 시작 요일과 동일한 경우만 참여 목록 따로 생성하는 예외 처리 필요
+        //  => 당일 시작 챌린지는 반드시 처음부터 상태를 진행 중으로 만들어야 함!
+        // 00시부터 시작하는 챌린지 -> 당일 시작하는 챌린지를 생성할 수 없다면 사용자 경험은? (다음 날부터 가능)
     }
 
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
