762aa2014741e2e6f63f4d6bcc43df65a0bfaa81
ssolpang <iless3377@naver.com>
2024-10-10 18:02:50 +0900

    feat/ 스케쥴러 메서드에 소요 시간 로그로 남기는 코드 추가 #272

--- src/main/java/com/habitpay/habitpay/domain/challengescheduler/application/ChallengeSchedulerService.java
+++ src/main/java/com/habitpay/habitpay/domain/challengescheduler/application/ChallengeSchedulerService.java
@@ -10,6 +10,7 @@ import com.habitpay.habitpay.domain.challengeparticipationrecord.domain.Challeng
 import com.habitpay.habitpay.domain.participationstat.dao.ParticipationStatRepository;
 import com.habitpay.habitpay.domain.participationstat.domain.ParticipationStat;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +21,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ChallengeSchedulerService {
 
     private final ChallengeRepository challengeRepository;
@@ -30,6 +32,10 @@ public class ChallengeSchedulerService {
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     public void setChallengeForStart() {
 
+        ZonedDateTime startTime = ZonedDateTime.now();
+        ZoneId timeZone = startTime.getZone();
+        log.info("setChallengeForStart started at : {}. TimeZone: {}", startTime, timeZone);
+
         // todo : 시작 날짜 ZonedDateTime으로 받고 있음 -> 날짜 데이터(without 시간)만으로 데이터 타입 바꾸기(프론트, 백 모두)
         List<Challenge> challengeList = schedulerTaskHelperService.findStartingChallenges();
 
@@ -37,11 +43,19 @@ public class ChallengeSchedulerService {
 
         schedulerTaskHelperService.createRecordsForChallenges(challengeList);
         challengeRepository.saveAll(challengeList);
+
+        ZonedDateTime endTime = ZonedDateTime.now();
+        Duration duration = Duration.between(startTime, endTime);
+        log.info("setChallengeForStart completed at : {}. Execution time: {}ms", endTime, duration.toMillis());
     }
 
     @Transactional
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     public void checkParticipationForChallenge() {
+
+        ZonedDateTime startTime = ZonedDateTime.now();
+        ZoneId timeZone = startTime.getZone();
+        log.info("checkParticipationForChallenge started at : {}. TimeZone: {}", startTime, timeZone);
 
         ZonedDateTime yesterday = ZonedDateTime.now().minusDays(1).with(LocalTime.MIDNIGHT);
         List<Challenge> challengeList = schedulerTaskHelperService.findChallengesForTodayParticipation(yesterday);
@@ -54,13 +68,25 @@ public class ChallengeSchedulerService {
 
         participationStatRepository.saveAll(failStatList);
         challengeParticipationRecordRepository.deleteAll(failRecordList);
+
+        ZonedDateTime endTime = ZonedDateTime.now();
+        Duration duration = Duration.between(startTime, endTime);
+        log.info("checkParticipationForChallenge completed at : {}. Execution time: {}ms", endTime, duration.toMillis());
     }
 
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     public void setChallengeForEnd() {
 
+        ZonedDateTime startTime = ZonedDateTime.now();
+        ZoneId timeZone = startTime.getZone();
+        log.info("setChallengeForEnd started at : {}. TimeZone: {}", startTime, timeZone);
+
         ZonedDateTime yesterday = ZonedDateTime.now().minusDays(1);
         List<Challenge> challengeList = schedulerTaskHelperService.findEndingChallenges(yesterday);
         challengeList.forEach(Challenge::setStateCompletedPendingSettlement);
+
+        ZonedDateTime endTime = ZonedDateTime.now();
+        Duration duration = Duration.between(startTime, endTime);
+        log.info("setChallengeForEnd completed at : {}. Execution time: {}ms", endTime, duration.toMillis());
     }
 }
