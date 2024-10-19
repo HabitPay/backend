029919b7d795b51b322ce4a96dab03de7c29ff75
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-10-19 23:39:14 +0900

    :recycle: refactor: 가독성 향상을 위한 getChallengeDetails 메서드 리팩토링 #288
    - Optional 메서드 체이닝을 이용해서 isParticipatedToday 변수 값 할당.
    - ChallengeParticipationRecordUtilService 의 메서드를 ChallengeParticipationRecordSearchService 메서드로 이동.

--- src/main/java/com/habitpay/habitpay/domain/challenge/application/ChallengeDetailsService.java
+++ src/main/java/com/habitpay/habitpay/domain/challenge/application/ChallengeDetailsService.java
@@ -4,7 +4,7 @@ import com.habitpay.habitpay.domain.challenge.domain.Challenge;
 import com.habitpay.habitpay.domain.challenge.dto.ChallengeDetailsResponse;
 import com.habitpay.habitpay.domain.challengeenrollment.dao.ChallengeEnrollmentRepository;
 import com.habitpay.habitpay.domain.challengeenrollment.domain.ChallengeEnrollment;
-import com.habitpay.habitpay.domain.challengeparticipationrecord.application.ChallengeParticipationRecordUtilService;
+import com.habitpay.habitpay.domain.challengeparticipationrecord.application.ChallengeParticipationRecordSearchService;
 import com.habitpay.habitpay.domain.member.domain.Member;
 import com.habitpay.habitpay.global.config.aws.S3FileService;
 import com.habitpay.habitpay.global.response.SuccessCode;
@@ -22,14 +22,17 @@ import java.util.Optional;
 public class ChallengeDetailsService {
     private final S3FileService s3FileService;
     private final ChallengeSearchService challengeSearchService;
-    private final ChallengeParticipationRecordUtilService challengeParticipationRecordUtilService;
+    private final ChallengeParticipationRecordSearchService challengeParticipationRecordSearchService;
     private final ChallengeEnrollmentRepository challengeEnrollmentRepository;
 
     public SuccessResponse<ChallengeDetailsResponse> getChallengeDetails(Long challengeId, Member member) {
         Challenge challenge = challengeSearchService.getChallengeById(challengeId);
         Optional<ChallengeEnrollment> optionalEnrollment = challengeEnrollmentRepository.findByMemberAndChallenge(member, challenge);
-        Boolean isMemberEnrolledInChallenge = optionalEnrollment.isPresent();
-        boolean isParticipatedToday = isMemberEnrolledInChallenge && challengeParticipationRecordUtilService.getIsParticipatedToday(optionalEnrollment.get());
+
+        boolean isMemberEnrolledInChallenge = optionalEnrollment.isPresent();
+        boolean isParticipatedToday = optionalEnrollment
+                .map(challengeParticipationRecordSearchService::hasParticipationRecord)
+                .orElseGet(() -> false);
         List<ChallengeEnrollment> challengeEnrollmentList = challengeEnrollmentRepository.findTop3ByChallenge(challenge);
         List<String> enrolledMembersProfileImageList = challengeEnrollmentList.stream()
                 .map((challengeEnrollment) -> {
