403d51e46e2a9d32c7a5e24d31a90845ba664d60
ssolpang <iless3377@naver.com>
2024-10-24 04:30:26 +0900

    feat/ 챌린지 내 전체 참여일 중에서 성공 및 실패 여부를 날짜별로 알려주는 getChallengeRecords 메서드 작성 #285

--- src/main/java/com/habitpay/habitpay/domain/challenge/application/ChallengeRecordsService.java
+++ src/main/java/com/habitpay/habitpay/domain/challenge/application/ChallengeRecordsService.java
@@ -4,12 +4,16 @@ import com.habitpay.habitpay.domain.challenge.domain.Challenge;
 import com.habitpay.habitpay.domain.challenge.dto.ChallengeRecordsResponse;
 import com.habitpay.habitpay.domain.challengeenrollment.application.ChallengeEnrollmentSearchService;
 import com.habitpay.habitpay.domain.challengeenrollment.domain.ChallengeEnrollment;
+import com.habitpay.habitpay.domain.challengeparticipationrecord.application.ChallengeParticipationRecordSearchService;
+import com.habitpay.habitpay.domain.challengeparticipationrecord.domain.ChallengeParticipationRecord;
 import com.habitpay.habitpay.domain.member.domain.Member;
+import com.habitpay.habitpay.global.config.timezone.TimeZoneConverter;
 import com.habitpay.habitpay.global.response.SuccessCode;
 import com.habitpay.habitpay.global.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,18 +24,31 @@ public class ChallengeRecordsService {
 
     private final ChallengeSearchService challengeSearchService;
     private final ChallengeEnrollmentSearchService challengeEnrollmentSearchService;
+    private final ChallengeParticipationRecordSearchService challengeParticipationRecordSearchService;
 
     public SuccessResponse<ChallengeRecordsResponse> getChallengeRecords(Long challengeId, Member member) {
         Challenge challenge = challengeSearchService.getChallengeById(challengeId);
         ChallengeEnrollment enrollment = challengeEnrollmentSearchService.getByMemberAndChallenge(member, challenge);
-        List<ZonedDateTime> requiredParticipationDates = challenge.getParticipationDates();
 
-        List<ZonedDateTime> successDayList = new ArrayList<>();
-        List<ZonedDateTime> failDayList = new ArrayList<>();
-        List<ZonedDateTime> upcomingDayList = new ArrayList<>();
+        List<LocalDate> successDayList = new ArrayList<>();
+        List<LocalDate> failDayList = new ArrayList<>();
+        List<LocalDate> upcomingDayList = new ArrayList<>();
 
-//        requiredParticipationDates.stream()
-//                .forEach(dates -> {});
+        List<ChallengeParticipationRecord> recordList = challengeParticipationRecordSearchService.findAllByChallengeEnrollment(enrollment);
+        LocalDate today = TimeZoneConverter.convertEtcToLocalTimeZone(ZonedDateTime.now()).toLocalDate();
+        recordList
+                .forEach(record -> {
+                    LocalDate targetDate = TimeZoneConverter.convertEtcToLocalTimeZone(record.getTargetDate()).toLocalDate();
+                    if (targetDate.isBefore(today)) {
+                        List<LocalDate> targetList = record.existsChallengePost() ? successDayList : failDayList;
+                        targetList.add(targetDate);
+                    } else if (targetDate.isEqual(today)) {
+                        List<LocalDate> targetList = record.existsChallengePost() ? successDayList : upcomingDayList;
+                        targetList.add(targetDate);
+                    } else {
+                        upcomingDayList.add(targetDate);
+                    }
+                });
 
         return SuccessResponse.of(
                 SuccessCode.NO_MESSAGE,
--- src/main/java/com/habitpay/habitpay/domain/challenge/dto/ChallengeRecordsResponse.java
+++ src/main/java/com/habitpay/habitpay/domain/challenge/dto/ChallengeRecordsResponse.java
@@ -3,13 +3,14 @@ package com.habitpay.habitpay.domain.challenge.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.util.List;
 
 @Getter
 @Builder
 public class ChallengeRecordsResponse {
-    List<ZonedDateTime> successDayList;
-    List<ZonedDateTime> failDayList;
-    List<ZonedDateTime> upcomingDayList;
+    List<LocalDate> successDayList;
+    List<LocalDate> failDayList;
+    List<LocalDate> upcomingDayList;
 }
