3371cde7e044bceef51b2b0dc75abb6c7b9b969e
ssolpang <iless3377@naver.com>
2024-11-05 01:30:22 +0900

    refact/ ChallengeRecords DTO의 불필요한 setter 어노테이션 삭제 #306

--- src/main/java/com/habitpay/habitpay/domain/challenge/dto/ChallengeRecords.java
+++ src/main/java/com/habitpay/habitpay/domain/challenge/dto/ChallengeRecords.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@Setter
 public class ChallengeRecords {
     private List<LocalDate> successDayList;
     private List<LocalDate> failureDayList;
