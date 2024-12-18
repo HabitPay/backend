277de574122d95978c62e9f34ab6a5f2b5e71187
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-12-18 21:37:09 +0900

    feat: 업로드 이미지 파일 크기 제한 1MB -> 10MB로 상향  #327

--- src/main/java/com/habitpay/habitpay/global/util/ImageUtil.java
+++ src/main/java/com/habitpay/habitpay/global/util/ImageUtil.java
@@ -3,14 +3,15 @@ package com.habitpay.habitpay.global.util;
 import com.habitpay.habitpay.domain.model.Image;
 
 public class ImageUtil {
+
     public static final long MB = 1024 * 1024;
-    public static final long PROFILE_IMAGE_SIZE_LIMIT = 1 * MB;
+    public static final long IMAGE_SIZE_LIMIT = 10 * MB;
 
     public static boolean isValidImageExtension(String extension) {
         return Image.VALID_EXTENSION.contains(extension.toUpperCase());
     }
 
     public static boolean isValidFileSize(Long size) {
-        return 0 < size && size <= PROFILE_IMAGE_SIZE_LIMIT;
+        return 0 < size && size <= IMAGE_SIZE_LIMIT;
     }
 }
